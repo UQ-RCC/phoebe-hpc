dac8c4771dfda53b07a1b5a46fcae0f52b99a6a3
Oliver Cairncross <op.cairncross@gmail.com>
2018-05-11 09:45:13 +1000

    Add project files.

---
+++ phoebe-hpc/phoebe-hpc.cpp
@@ -0,0 +1,12 @@
+﻿// phoebe-hpc.cpp : Defines the entry point for the application.
+//
+
+#include "phoebe-hpc.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
