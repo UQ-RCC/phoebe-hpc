11f3aa2373ed912c2dac690d90b4506b67f7b039
Oliver Cairncross <op.cairncross@gmail.com>
2018-05-14 09:12:24 +1000

    test boost options

--- ImportTargets.cmake
+++ ImportTargets.cmake
@@ -2,7 +2,7 @@
 
 function(create_boost_target)
 
-	set(BOOST_OPTIONS thread system date_time regex chrono)
+	# set(BOOST_OPTIONS thread system date_time regex chrono)
 	set(BOOST_LIB_PREFIX "libboost_")
 
 	if(WIN32)	
