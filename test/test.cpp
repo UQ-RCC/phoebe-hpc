114e572c22e431415cdc9afb750030d3f02b5215
Oliver Cairncross <op.cairncross@gmail.com>
2018-05-14 10:27:03 +1000

    create test code

--- test/test.cpp
+++ test/test.cpp
@@ -1,14 +1,15 @@
 ﻿// phoebe-hpc.cpp : Defines the entry point for the application.
 //
 
-#include "phoebe-hpc.h"
+#include "test.h"
 #include <boost/version.hpp>
 
 int main()
 {
+	std::cout << "Testing Phoebe components...\n\n";
 	std::cout << "boost lib version: " << BOOST_LIB_VERSION << std::endl;
 #ifdef _WIN32
-	std::cout << "Enter to exit" << std::endl;
+	std::cout << "\nEnter to exit" << std::endl;
 	std::cin.get();
 #endif // _WIN32
 	return 0;
